4f9b1ed2cddfe0be5e509c4d5b360538242465c8
Maximilian Kapra <maximilian@kapra.de>
2021-08-29 21:50:51 +0200

    Add emacs configs

---
+++ emacs/init.el
@@ -0,0 +1,3 @@
+# ~/.emacs.d/init.el
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
---
+++ emacs/macos_local.el
@@ -0,0 +1,5 @@
+# ~/.emacs.d/local.el
+
+;; Automatic theme switching
+(use-package auto-dark-emacs
+  :straight (auto-dark-emacs :type git :host github :repo "LionyxML/auto-dark-emacs"))
