611f1e76703141c8b3b0156441d4e201f6cf6f4a
Maximilian Kapra <maximilian@kapra.de>
2023-06-21 21:04:53 +0200

    [uninstallations.sh] Add missing -r

--- uninstallations.sh
+++ uninstallations.sh
@@ -6,4 +6,4 @@ echo "-- Removing lazygit"
 find "${HOME}" -type f -not -path "*/*dotfiles*/*" -name "*lazygit*" -exec rm {} + 2> /dev/null
 
 echo "-- Removing nvim"
-rm -f "${HOME}/.config/nvim"
+rm -rf "${HOME}/.config/nvim"
