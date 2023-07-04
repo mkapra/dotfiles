565c655a2f283d0ef3009bd9662bbda5795b536a
Maximilian Kapra <maximilian_kapra@genua.de>
2023-07-04 13:47:29 +0200

    [ansible] update gitignore, remove gnome-terminal

--- setup.sh
+++ setup.sh
@@ -9,50 +9,8 @@ install_kitty_terminal() {
   then
     mkdir -p $HOME/.local/bin
     ln -fs $SCRIPT/terminals/nightlight.sh $HOME/.local/bin/
-    ln -fs $SCRIPT/submodules/kitty-rose-pine-theme/icons/rose-pine-terminal-icon.png $HOME/.config/kitty/kitty.app.png
-
-    if [[ "$(uname -a)" == *"Darwin"* ]]
-    then
-        rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
-    fi
   fi
 }
-
-# ============================== gnome-terminal
-install_gnome_terminal() {
-  # shortcuts
-  shortcuts() {
-    if which gnome-terminal &> /dev/null
-    then
-      gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
-      gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
-
-      for i in {1..10..1}
-      do
-        gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ switch-to-tab-$i ''
-      done
-    fi
-  }
-
-  # color scheme
-  gt_colorscm() {
-    if which gnome-terminal &> /dev/null
-    then
-      echo "-- Install everforest gnome-terminal theme"
-      mkdir -p $HOME/.local/bin
-      ln -fs $SCRIPT/terminals/nightlight.sh $HOME/.local/bin/
-      dconf reset -f /org/gnome/terminal/legacy/profiles:/
-      dconf load / < $SCRIPT/terminals/gnome-terminal/everforest-dark-hard-theme.dconf
-      dconf load / < $SCRIPT/terminals/gnome-terminal/everforest-light-hard-theme.dconf
-      dconf write /org/gnome/terminal/legacy/default-profile "'b1dcc9dd-5262-4d8d-a863-c897e6d979b9'"
-
-    fi
-  }
-
-  shortcuts
-  gt_colorscm
-}
-
 
 # ============================== shells
 install_shells() {
@@ -76,8 +34,6 @@ install_shells() {
 }
 
 
-install_fonts
-install_gnome_terminal
 install_kitty_terminal
 install_shells
 
