f1bb726fa1f605e1694fafc2e1c0368563f4637a
Maximilian Kapra <maximilian_kapra@genua.de>
2023-07-04 14:03:01 +0200

    [ansible] Add shell configs and remove setup.sh

--- uninstallations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "## Cleanup old configuration/tools ##"
-
-echo "-- Removing lazygit"
-find "${HOME}" -type f -not -path "*/*dotfiles*/*" -name "*lazygit*" -exec rm {} + 2> /dev/null
-
-echo "-- Removing nvim"
-rm -rf "${HOME}/.config/nvim" "${HOME}/.cache/*nvim*" "${HOME}/.local/share/nvim"
