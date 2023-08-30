69f026818167996bcb149d725c0f59d1a56fc491
Maximilian Kapra <maximilian_kapra@genua.de>
2023-08-30 20:02:24 +0200

    [kitty/hx/nightlight] Refactor nightlight with dark theme

--- terminals/nightlight.sh
+++ terminals/nightlight.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-# Define the dark and light profile UUIDs
-dark_profile="358f6041-d4a0-4de3-90f3-e4afc6252ea1"
-light_profile="b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
-
 # Get the current option
 option=$1
 
 # Function to change the profile of all open kitty-terminal windows
 change_profile() {
-    if [[ $1 == 'moon' ]]
+    if [[ $1 == 'dark' ]]
     then
-        ln -fs $HOME/.config/kitty/onehalf-dark.conf $HOME/.config/kitty/current-theme.conf
+        ln -fs $HOME/.config/kitty/ayu_mirage.conf $HOME/.config/kitty/current-theme.conf
+        sed -i 's/theme = ".*"/theme = "ayu_mirage"/' $HOME/.config/helix/config.toml
     else
-        ln -fs $HOME/.config/kitty/rose-pine-$1.conf $HOME/.config/kitty/current-theme.conf
+        ln -fs $HOME/.config/kitty/rose-pine-dawn.conf $HOME/.config/kitty/current-theme.conf
+        sed -i 's/theme = ".*"/theme = "rose_pine_dawn"/' $HOME/.config/helix/config.toml
     fi
+
+    while read -r host; do
+        if [[ $1 == 'dark' ]]
+        then
+            ssh $host touch .darkmode
+            ssh $host bash <<EOF
+                if [[ -f $HOME/.config/helix/config.toml ]]
+                then
+                    sed -i 's/theme = ".*"/theme = "ayu_mirage"/' $HOME/.config/helix/config.toml
+                fi
+EOF
+        else
+            ssh $host rm -f $host
+            ssh $host bash <<EOF
+                if [[ -f $HOME/.config/helix/config.toml ]]
+                then
+                    sed -i 's/theme = ".*"/theme = "rose_pine_dawn"/' $HOME/.config/helix/config.toml
+                fi
+EOF
+        fi
+    done < $HOME/.ssh/servers
+
     files="/tmp/kitty-$USER-*"
     for instance in $files
     do
@@ -31,44 +51,11 @@ fi
 set -x
 # Check if option is "dark" or "light"
 if [ "$option" == "dark" ]; then
-    change_profile moon
-
     touch $HOME/.darkmode
-    sed -i 's/theme = ".*"/theme = "rose_pine_moon"/' ${HOME}/.config/helix/config.toml
-    # pkill -USR1 hx
-    while read -r host; do
-      scp -q .darkmode $host:~
-      
-      ssh $host bash <<EOF
-        if [[ -f $HOME/.config/helix/config.toml ]]
-        then
-            sed -i 's/theme = ".*"/theme = "rose_pine_moon"/' ${HOME}/.config/helix/config.toml
-        fi
-        if [[ -f $HOME/.cargo/bin/zellij ]]
-        then
-            sed -i 's/theme ".*"/theme "rose-pine-moon"/' ${HOME}/.config/zellij/config.kdl
-        fi
-EOF
-    done < "$HOME/.ssh/servers"
+    change_profile "dark"
 elif [ "$option" == "light" ]; then
-    change_profile dawn
-
-    rm $HOME/.darkmode
-    sed -i 's/theme = ".*"/theme = "rose_pine_dawn"/' ${HOME}/.config/helix/config.toml
-    # pkill -USR1 hx
-    while read -r host; do
-      ssh $host bash <<EOF
-        rm ~/.darkmode
-        if [[ -f $HOME/.config/helix/config.toml ]]
-        then
-            sed -i 's/theme = ".*"/theme = "rose_pine_dawn"/' ${HOME}/.config/helix/config.toml
-        fi
-        if [[ -f $HOME/.cargo/bin/zellij ]]
-        then
-            sed -i 's/theme ".*"/theme "rose-pine-dawn"/' ${HOME}/.config/zellij/config.kdl
-        fi
-EOF
-    done < "$HOME/.ssh/servers"
+    rm -f $HOME/.darkmode
+    change_profile
 else
     echo "Invalid option. Please use 'dark' or 'light'."
 fi
